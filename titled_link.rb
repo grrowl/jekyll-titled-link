70938f5daa691508187369838e568d5d229925d8
Tom McKenzie <tom@chillidonut.com>
2015-12-06 17:17:50 +1100

    Heaps of updates and fixes

--- titled_link.rb
+++ titled_link.rb
@@ -1,95 +1,96 @@
-# A Liquid tag for Jekyll sites that allows embedding tweets using Twitter's
-# oEmbed API, and showing the tweet as a blockquote for non-JavaScript enabled
-# browsers and readers.
+# Embeds links with their source document titles.
 #
-# Author: Scott W. Bradley
-# Source URL: https://github.com/scottwb/jekyll-tweet-tag
+# Adapted from Tweet Tag:
+#   Author: Scott W. Bradley
+#   Source URL: https://github.com/scottwb/jekyll-tweet-tag
 #
 # Example usage:
-#   {% tweet https://twitter.com/DEVOPS_BORAT/status/159849628819402752 %}
+#   {% titled_link https://www.theguardian.com/ %}
 #
 # Documentation:
-#   https://github.com/scottwb/jekyll-tweet-tag/blob/master/README.md
+#   https://github.com/grrowl/jekyll-titled-link/blob/master/README.md
 #
+
 require 'json'
 require 'net/http'
 
 module Jekyll
-  class TweetTag < Liquid::Tag
-
-    TWITTER_OEMBED_URL = "https://api.twitter.com/1/statuses/oembed.json"
+  class TitledTag < Liquid::Tag
 
     def initialize(tag_name, text, tokens)
       super
       @text           = text
       @cache_disabled = false
-      @cache_folder   = File.expand_path "../.tweet-cache", File.dirname(__FILE__)
+      @cache_folder   = File.expand_path "../.link-cache", File.dirname(__FILE__)
       FileUtils.mkdir_p @cache_folder
     end
 
     def render(context)
-      args       = context['tweet'].split(/\s+/).map(&:strip)
-      api_params = {'url' => args.shift}
+      uri = context[@text] || @text
 
-      args.each do |arg|
-        k,v = arg.split('=').map(&:strip)
-        if k && v
-          if v =~ /^'(.*)'$/
-            v = $1
-          end
-          api_params[k] = v
-        end
-      end
+      title = cached_response(uri) || live_response(uri) || uri
+      title = uri if title == ""
 
-      html_output_for(api_params)
+      "<a href=\"#{uri}\" target=\"_blank\">#{title}</a>"
     end
 
-    def html_output_for(api_params)
-      body = "Tweet could not be processed"
-      if response = cached_response(api_params) || live_response(api_params)
-        body = response['html'] || response['error'] || body
-      end
-      "<div class='embed tweet'>#{body}</div>"
-    end
-
-    def cache(api_params, data)
-      cache_file = cache_file_for(api_params)
-      File.open(cache_file, "w") do |f|
-        f.write(data)
+    def cache(uri, data)
+      cache_file = cache_file_for(uri)
+      File.open(cache_file, "w:UTF-8") do |f|
+        f.write({
+          uri: uri,
+          data: data
+        }.to_json)
       end
     end
 
-    def cached_response(api_params)
+    def cached_response(uri)
       return nil if @cache_disabled
-      cache_file = cache_file_for(api_params)
-      JSON.parse(File.read(cache_file)) if File.exist?(cache_file)
+      cache_file = cache_file_for(uri)
+      JSON.parse(File.read(cache_file))['data'] if File.exist?(cache_file)
     end
 
-    def url_params_for(api_params)
-      api_params.keys.sort.map do |k|
-        "#{CGI::escape(k)}=#{CGI::escape(api_params[k])}"
-      end.join('&')
-    end
-
-    def cache_file_for(api_params)
-      filename = "#{Digest::MD5.hexdigest(url_params_for(api_params))}.cache"
+    def cache_file_for(uri)
+      filename = "#{Base64.urlsafe_encode64(uri)}.cache"
       File.join(@cache_folder, filename)
     end
 
-    def live_response(api_params)
-      api_uri = URI.parse(TWITTER_OEMBED_URL + "?#{url_params_for(api_params)}")
-      http = Net::HTTP.new(api_uri.host, api_uri.port)
-      http.use_ssl = true
-      request = Net::HTTP::Get.new(api_uri.request_uri)
-      response = http.request(request)
+    def get_title_from(html)
+      title_tag = /<title>(.+?)<\/title>/.match(html)
+
+      return (title_tag && title_tag[1]) || ""
+    end
+
+    def live_response(uri)
+      uri = URI(uri)
+      response = nil
+      puts "Fetching title of %s"% uri
+
+      begin
+        Net::HTTP.start(uri.host, uri.port,
+          :continue_timeout => 3,
+          :read_timeout => 3,
+          :use_ssl => uri.scheme == 'https'
+          ) do |http|
+          request = Net::HTTP::Get.new uri.request_uri
+
+          response = http.request request # Net::HTTPResponse object
+        end
+      rescue TimeoutError
+        puts "! Timeout fetching %s"% uri.host
+      rescue
+        puts "! Error fetching %s: %s"% [uri.host, $!, $!.class]
+      end
 
       return unless response.class <= Net::HTTPSuccess
-      cache(api_params, response.body) unless @cache_disabled
-      JSON.parse(response.body)
+
+      title = get_title_from(response.body).force_encoding("UTF-8")
+      cache(uri.to_s, title) unless @cache_disabled
+      title
     end
   end
 
-  class TweetTagNoCache < TweetTag
+  class TitledTagNoCache < TitledTag
     def initialize(tag_name, text, token)
       super
       @cache_disabled = true
@@ -97,5 +98,5 @@ module Jekyll
   end
 end
 
-Liquid::Template.register_tag('tweet', Jekyll::TweetTag)
-Liquid::Template.register_tag('tweetnocache', Jekyll::TweetTagNoCache)
+Liquid::Template.register_tag('titled_link', Jekyll::TitledTag)
+Liquid::Template.register_tag('titled_linknocache', Jekyll::TitledTagNoCache)
